080eeeea387f0d983f7f72fee5ae2625a5b01532
Mihai Niculae <85626030+mihainrs@users.noreply.github.com>
2024-10-21 13:25:26 +0300

    done :dolphin:

--- main.js
+++ main.js
@@ -14,9 +14,44 @@ const mockUpStrand = () => {
 };
 
 
+const pAequorFactory = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate() {
+      const index = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+      while (this.dna[index] === newBase) {
+        newBase = returnRandBase();
+      }
+      this.dna[index] = newBase;
+      return this.dna;
+    },
+    compareDNA(otherPAequor) {
+      let common = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === otherPAequor.dna[i]) {
+          common++;
+        }
+      }
+      const percentage = (common / this.dna.length) * 100;
+      console.log(`specimen #${this.specimenNum} and specimen #${otherPAequor.specimenNum} have ${percentage.toFixed(2)}% DNA in common.`);
+    },
+    willLikelySurvive() {
+      const count = this.dna.filter(base => base === 'C' || base === 'G').length;
+      return (count / this.dna.length) >= 0.6;
+    }
+  };
+};
 
+const survivingSpecimens = [];
+let id = 1;
+while (survivingSpecimens.length < 30) {
+  const newOrganism = pAequorFactory(id, mockUpStrand());
+  if (newOrganism.willLikelySurvive()) {
+    survivingSpecimens.push(newOrganism);
+  }
+  id++;
+}
 
-
-
-
-
+// console.log(mockUpStrand());
